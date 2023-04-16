8526ebb667c20ab990f1ec411f778dbd5af29b55
ismat27 <adegbiteismail27@gmail.com>
2023-04-16 22:10:53 +0100

    update form validation

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,24 +1,61 @@
-document.querySelector(".submit").addEventListener("click", (e)=>{
-    e.preventDefault();
+// const passwordInput = document.querySelector('input[name=password]')
+// const confirmPasswordInput = document.querySelector('input[name=confirmPassword]')
+// const nameInput = document.querySelector('input[name=name]')
+// const emailInput = document.querySelector('input[name=email]')
+// const phoneInput = document.querySelector('input[name=phoneNumber]')
+
+// const form = document.querySelector('form')
+
+// passwordInput.addEventListener('input', ()=> {
+
+// })
+
+function validateInputs() {
     let input = document.querySelectorAll("input");
     let err = document.querySelector(".error");
-    let password = document.querySelector(".password");
-    let confirmPassword = document.querySelector(".confirm-password");
     let pass_err = document.querySelector(".pass-err")
-    input.forEach((inp)=>{
-        if (inp.value == ""){
+
+    const passwordInput = document.querySelector('input[name=password]')
+    const confirmPasswordInput = document.querySelector('input[name=confirmPassword]')
+    const nameInput = document.querySelector('input[name=name]')
+    const emailInput = document.querySelector('input[name=email]')
+    const phoneInput = document.querySelector('input[name=phoneNumber]')
+        
+    input.forEach( (inp) => {
+        if (!inp.value.trim()){
+            console.log(inp.value);
             inp.style.borderColor = "red"
-                err.style.display = "block"
-        }else if(password != confirmPassword){
-            confirmPassword.style.borderColor = "red"
-            pass_err.style.display = "block"
-        }
-        else{
-            inp.style.borderColor = "green"
-            err.style.display ="none";
+            err.style.display = "block"
+            return false
         }
     });
 
-    e.curre
-
-})
+    if (nameInput.value === '') {
+        return false
+    }
+    if (phoneInput.value === '') {
+        return false
+    }
+    if (emailInput.value === '') {
+        return false
+    }
+    if (passwordInput.value === '') {
+        return false
+    }
+    if (confirmPasswordInput.value === '') {
+        return false
+    }
+    if (passwordInput.value !== confirmPasswordInput.value) {
+        confirmPasswordInput.style.borderColor = "red"
+        passwordInput.style.borderColor = "red"
+        pass_err.style.display = "block"
+        console.log('passwords not equal');
+        return false
+    }
+    else {
+        console.log('ok');
+        confirmPasswordInput.style.borderColor = "green"
+        passwordInput.style.borderColor = "green"
+        return true
+    }
+}
