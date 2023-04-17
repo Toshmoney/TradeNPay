db52d8a275f3a4609f71c474ad73af8efd117fa1
ismat27 <adegbiteismail27@gmail.com>
2023-04-17 11:26:02 +0100

    only allow authenticated users access to dashboard

--- routes/handler.js
+++ routes/handler.js
@@ -7,6 +7,12 @@ const {
     receiveWallet, setting, profile, verifyNow, test 
 } = require("../controller/controller")
 
+// middlewares
+
+const {
+    isLoggedIn
+} = require("../middleware/authenticate")
+
 const {
     newUser,
     signUpPage,
@@ -18,20 +24,20 @@ const {
 router.route("/").get(homePage)
 router.route("/test").get(test)
 router.route("/login").get(logInPage)
-router.route("/login").post(passport.authenticate('local', { failureRedirect: '/login' }), login)
+router.route("/login").post(passport.authenticate('local', { failureRedirect: '/login', failureFlash:'incorrect credientials', failureMessage: true }), login)
 router.route('/logout').get(logout)
 router.route("/sign-up").get(signUpPage)
 router.route("/sign-up").post(newUser)
-router.route("/dashboard").get(dashboard)
+router.route("/dashboard").get(isLoggedIn, dashboard)
 router.route("/dashboard").post(dashboard)
-router.route("/airtime").get(airtime)
-router.route("/data").get(dataplan)
-router.route("/billpayment").get(billpayment)
-router.route("/wallet").get(wallet)
+router.route("/airtime").get(isLoggedIn, airtime)
+router.route("/data").get(isLoggedIn, dataplan)
+router.route("/billpayment").get(isLoggedIn, billpayment)
+router.route("/wallet").get(isLoggedIn, wallet)
 router.route("/wallet/fund").get(fundWallet);
 router.route("/wallet/receive").get(receiveWallet);
-router.route("/setting").get(setting);
-router.route("/profile").get(profile);
+router.route("/setting").get(isLoggedIn, setting);
+router.route("/profile").get(isLoggedIn, profile);
 router.route("/verify_now").get(verifyNow);
 
 module.exports = router;
