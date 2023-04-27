c72ab1a18f1b8e49bf496228f91c47daef9fa043
ismat27 <adegbiteismail27@gmail.com>
2023-04-27 20:00:18 +0100

    update procedure for purchasing tv subscription

--- routes/tvRouter.js
+++ routes/tvRouter.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const verifyWallet = require("../middleware/checkUserWallet")
+const { validateCustomer } = require("../middleware/validateCustomer")
+
 const {
     rechargeTv
 } = require("../controller/tvController")
 
 const router = express.Router()
 
-router.route('/recharge').post(rechargeTv)
+router.route('/recharge').post([verifyWallet, validateCustomer], rechargeTv)
 
 module.exports = router
