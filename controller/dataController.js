06838cdfc1d738b523f8e40cfd268b74ecaf9c12
ismat27 <adegbiteismail27@gmail.com>
2023-05-01 17:22:40 +0100

    use middleware for checking user available balance against transaction amount

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const { fetchPrices, generateTransId } = require("../utils")
-const Wallet = require('../model/Wallet')
 const Transaction = require("../model/Transaction")
 const { default: axios } = require("axios")
 
@@ -29,19 +28,8 @@ const buyData = async (req, res) => {
         service_type
     } = req.body
     const user = req.user
-    if (!user) {
-        return res.status(401).json({
-            message: 'Pls log in'
-        })
-    }
     // check if user has enough in his wallet
-    const userWallet = await Wallet.findOne({ user: user._id })
-    if (!userWallet || userWallet.balance < Number(amount)) {
-        return res.status(400).json({
-            message: 'not enough in your wallet'
-        })
-    }
-
+    const userWallet = req.user.wallet
     // create a unique transaction_id
     let transaction_id;
     while (true) {
