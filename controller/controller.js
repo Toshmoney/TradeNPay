d5c6850b28d5ac557a9eefc3bd0dd476af5e211b
Toshmoney <abdulmaleeqomotosho@gmail.com>
2023-06-06 00:32:46 +0100

    Fixing buy data and controller bugs

--- controller/controller.js
+++ controller/controller.js
@@ -1,65 +1,6 @@
-<<<<<<< HEAD
 require('dotenv').config();
-const {User} = require("../model/User.db")
 const key = process.env.pub_key;
-const Wallet = require("../model/Wallet")
-const Transaction = require("../model/Transaction")
-
-const formatTransactionDate = (date_string) => {
-    const trans_date = new Date(date_string)
-    const year = trans_date.getFullYear()
-    let month = trans_date.getMonth() + 1
-    let day = trans_date.getDate()
-    if (month < 10) {
-        month = `0${month}`
-    }
-    if (day < 10) {
-        day = `0${day}`
-    }
-    return `${year}-${month}-${day}`
-}
-
-const fetchUserTransactions = async (userId, limit=20) => {
-    let transactions = await Transaction.find({user: userId})
-    .sort('-createdAt')
-    .limit(limit)
-    if (transactions.length === 0) {
-        return []
-    }
-    transactions = transactions.map(transaction => {
-        const item = transaction.toObject()
-        return {
-            ...item,
-            createdAt: formatTransactionDate(item.createdAt),
-            updatedAt: formatTransactionDate(item.updatedAt),
-        }
-    })
-    return transactions
-}
-
-const dashboardData = async (user) => {
-    const userWallet = await Wallet.findOne({
-        user: user._id
-    })
-    const userTransactions = await fetchUserTransactions(user._id)
-    const user_data = {
-        name: user.name,
-        email: user.email,
-    }
-    const data = {
-        user: user_data,
-        transactions: userTransactions
-    }
-    if (userWallet) {
-        user_data.balance = userWallet.balance
-        data.balance = userWallet.balance
-    }
-    return data
-}
-=======
 const { dashboardData } = require("../utils/dashboardData")
->>>>>>> bc67ef13adbe7dad829c64297b14afe8d6fb3413
-
 const {
     fetchPrices
 } = require('../utils')
