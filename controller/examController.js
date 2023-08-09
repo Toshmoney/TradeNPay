9fef23e170d4a18d6a0163824b3cf09bc2295bec
ismat27 <adegbiteismail27@gmail.com>
2023-08-09 12:42:32 +0100

    updates

--- controller/examController.js
+++ controller/examController.js
@@ -1,90 +1,97 @@
-require("dotenv").config()
-const { default: axios } = require("axios")
-const Transaction = require("../model/Transaction")
-const { generateTransId } = require("../utils")
+require("dotenv").config();
+const { default: axios } = require("axios");
+const Transaction = require("../model/Transaction");
+const { generateTransId } = require("../utils");
 
 const recharge = async (req, res) => {
-    const user = req.user
-    const { service_id, amount, quantity } = req.body
-    
-    if (!service_id) {
-        return res.status(400).json({
-            message: 'missing provider',
-            error: 'service_id'
-        })
+  const user = req.user;
+  const { service_id, amount, quantity } = req.body;
+
+  if (!service_id) {
+    return res.status(400).json({
+      message: "missing provider",
+      error: "service_id",
+    });
+  }
+
+  // check if user has enough in his wallet
+  const userWallet = req.user.wallet;
+  const userBalance = userWallet.current_balance;
+
+  let transaction_id;
+  while (true) {
+    transaction_id = generateTransId();
+    const existingTrans = await Transaction.findOne({
+      reference_number: transaction_id,
+    });
+    if (!existingTrans) {
+      break;
     }
-   
-    // check if user has enough in his wallet
-    const userWallet = req.user.wallet
-    
-    let transaction_id;
-    while (true) {
-        transaction_id = generateTransId()
-        const existingTrans = await Transaction.findOne({ reference_number: transaction_id })
-        if (!existingTrans) {
-            break
-        }
+  }
+  // create transaction with pending status
+  const transaction = new Transaction({
+    user: user._id,
+    balance_before: userBalance,
+    balance_after: userBalance,
+    amount: Number(amount),
+    status: "pending",
+    service: "exam",
+    type: "debit",
+    description: `Result checker purchase`,
+    reference_number: transaction_id,
+  });
+  // If validation is successful make data to proceed to purchase
+  const rechargeRequestData = {
+    service_id,
+    trans_id: transaction_id,
+    quantity,
+    amount: Number(amount),
+  };
+  console.log("recharge request data: ", rechargeRequestData);
+  try {
+    // send request to purchase
+    console.log("purchase started");
+    const rechargeResponse = await axios.post(
+      "https://enterprise.mobilenig.com/api/services/",
+      rechargeRequestData,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const rechargeResponseData = rechargeResponse.data;
+    console.log(rechargeResponseData);
+    if (rechargeResponseData.message !== "success") {
+      console.log("purchase failed");
+      return res.status(400).json({
+        message: rechargeResponseData.details,
+        error: "recharge",
+      });
     }
-    // create transaction with pending status
-    const transaction = new Transaction({
-        user: user._id,
-        wallet: userWallet,
-        amount: Number(amount),
-        status: 'pending',
-        type: 'debit',
-        description: `Result checker purchase`,
-        reference_number: transaction_id
-    })
-    // If validation is successful make data to proceed to purchase
-    const rechargeRequestData = {
-        service_id,
-        trans_id: transaction_id,
-        quantity,
-        amount: Number(amount)
-    }
-    console.log('recharge request data: ', rechargeRequestData);
-    try {
-        // send request to purchase
-        console.log('purchase started');
-        const rechargeResponse = await axios.post(
-            'https://enterprise.mobilenig.com/api/services/', 
-            rechargeRequestData,
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.API_SECRET_KEY}`,
-                    "Content-Type": 'application/json'
-                }
-            }
-        )
-        const rechargeResponseData = rechargeResponse.data
-        console.log(rechargeResponseData);
-        if (rechargeResponseData.message !== 'success') {
-            console.log('purchase failed');
-            return res.status(400).json({
-                message: rechargeResponseData.details,
-                error: 'recharge'
-            })
-        }
-        console.log('purchase completed');
-        // update transaction to be concluded
-        transaction.status = 'completed'
-        // deduct transaction amount from user wallet
-        userWallet.balance = userWallet.balance - Number(amount)
-        await userWallet.save()
-        await transaction.save()
-        res.status(202).json({
-            message: 'your transaction is being processed',
-            balance: userWallet.balance
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(422).json({
-            message: 'failed',
-            error: 'validation'
-        })
-    }
-}
+    console.log("purchase completed");
+    // update transaction to be concluded
+    transaction.status = "completed";
+    transaction.balance_after = userBalance - Number(amount);
+    // deduct transaction amount from user wallet
+    userWallet.previous_balance = userBalance;
+    userWallet.current_balance = userBalance - Number(amount);
+    await userWallet.save();
+    await transaction.save();
+    res.status(202).json({
+      message: "your transaction is being processed",
+      balance: userWallet.balance,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(422).json({
+      message: "failed",
+      error: "validation",
+    });
+  }
+};
 
 module.exports = {
-    buyExamPin: recharge
-}
+  buyExamPin: recharge,
+};
