d44c581a7c6417ec37ebede0f5d5c038be99d35f
ismail adegbite <adegbiteismail27@gmail.com>
2023-08-30 11:42:33 +0100

    added middleware for checking user

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,13 +1,27 @@
+const { CustomAPIError } = require("../handleError");
+
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    req.session.requestedUrl = req.originalUrl
-    const requestedUrl = req.originalUrl || '/dashboard'
-    req.flash('info', 'your session has expired pls log in')
-    res.redirect(`/login?redirect=${requestedUrl}`);
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.session.requestedUrl = req.originalUrl;
+  const requestedUrl = req.originalUrl || "/dashboard";
+  req.flash("info", "your session has expired pls log in");
+  res.redirect(`/login?redirect=${requestedUrl}`);
+}
+
+function isAdmin(req, res, next) {
+  if (!req.isAuthenticated()) {
+    throw new CustomAPIError("session expired, login to continue", 401);
+  }
+  // check if user is admin
+  if (req.user.is_admin) {
+    return next();
+  }
+  throw new CustomAPIError("unauthorized user", 403);
 }
 
 module.exports = {
-    isLoggedIn
-}
+  isLoggedIn,
+  isAdmin,
+};
